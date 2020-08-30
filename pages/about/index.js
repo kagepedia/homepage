ab1e209edf590b02e4e5a1443522282777d29439
Kage Soichiro <k4gepedia@gmail.com>
2020-08-31 00:09:09 +0900

    about page create

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,14 +1,28 @@
 import Head from "../../components/head";
+import Header from "../../components/molecules/header";
 
 const About = () => (
   <div>
     <Head />
-    <div className="p-4 bg-white rounded shadow">
-      <h1 className="text-2xl font-bold">About Me</h1>
-      <p className="text-gray-500">
-        with Tailwind CSS & postcss-preset-env. Now uses tailwind purge method
-        for production builds.
-      </p>
+    <Header />
+    <div className="about-page">
+      <div className="p-4 bg-white rounded shadow">
+        <h1 className="text-2xl font-bold text-center">About Me</h1>
+        <img src="/assets/img/profile.JPG"></img>
+        <p className="text-gray-500 text-center">
+          WEB制作会社でプログラマーをやっている24歳です。
+          <br />
+          当ブログでは、自分が技術を通して得た知見を主に発信していきます。
+          <br />
+          趣味は、旅行・写真・ゲームです。
+        </p>
+        <p className="test">test</p>
+      </div>
+      <style jsx>{`
+        .test {
+          color: red;
+        }
+      `}</style>
     </div>
   </div>
 );
