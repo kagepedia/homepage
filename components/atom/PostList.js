62a87bc0f09a6a28e20bb0bdec976c392ed07cea
Kage Soichiro <k4gepedia@gmail.com>
2020-09-07 00:21:34 +0900

    update

--- components/atom/PostList.js
+++ components/atom/PostList.js
@@ -16,12 +16,14 @@ const PostList = () => (
               >
                 <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
               </svg>
-              Members only
+              2020-10-30
             </p>
             <div className="text-gray-900 font-bold text-xl mb-2">
-              Can coffee make you a better developer?
+              Article Title
             </div>
             <p className="text-gray-700 text-base">
+              article description.
+              <br />
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
               Voluptatibus quia, nulla! Maiores et perferendis eaque,
               exercitationem praesentium nihil.
