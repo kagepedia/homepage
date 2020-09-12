d8c39c34485aff4ac82cc4cdb980cbbc76e0e53b
Kage Soichiro <k4gepedia@gmail.com>
2020-09-12 23:38:47 +0900

    GAタグ導入

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,24 @@
+import Router from 'next/router';
+import * as gtag from '../utils/gtag';
 import '../public/css/main.css';
+import { useEffect } from 'react';
 
 function App({ Component, pageProps }) {
+  useEffect(() => {
+    if (!gtag.existsGaId) {
+      return;
+    }
+
+    const handleRouteChange = (path) => {
+      gtag.pageview(path);
+    };
+
+    Router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
