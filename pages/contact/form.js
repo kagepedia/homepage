c2ebe9a443c9862920f55511ca5b8e63726e15ea
Kage Soichiro <k4gepedia@gmail.com>
2020-09-17 07:07:00 +0900

    not yet deploy

--- pages/contact/form.js
+++ pages/contact/form.js
@@ -1,28 +1,14 @@
-import fetch from 'node-fetch';
 import Head from '../../components/head';
 import Header from '../../components/molecules/header';
 import Footer from '../../components/molecules/footer';
-
-// SendGrid
-fetch('/api/sendmail', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({
-    email: 'sample@gmail.com',
-    message: 'Test mail2.',
-  }),
-});
-// SendGrid
 
 const Form = () => (
   <div>
     <Head title={'お問い合わせ（入力）'} url={'https://kagepedia.com/contact/form'} />
     <Header />
     <div className="p-4 bg-white rounded shadow">
-      <h1 className="text-2xl font-bold">SendGrid Contact Form</h1>
-      <p className="text-gray-500">SendGrid</p>
+      <h1 className="text-2xl font-bold">Contact Form</h1>
+      <p className="text-gray-500">Contact Form</p>
     </div>
     <Footer />
   </div>
