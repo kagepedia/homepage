77ee0a4ad5ba26e790f5115683a194c2527a3ebe
Kage Soichiro <k4gepedia@gmail.com>
2020-09-16 12:22:22 +0900

    SendGridお問い合わせフォーム作成

---
+++ pages/api/sendmail.js
@@ -0,0 +1,27 @@
+// SendGrid
+/*
+const sgMail = require('@sendgrid/mail');
+
+export default async function (req, res) {
+  sgMail.setApiKey('SendGridで作成したAPIキー');
+
+  const { email, message } = req.body;
+
+  const content = {
+    to: email,
+    from: '任意のメールアドレス（実現するもの）',
+    subject: 'メールのタイトルです',
+    text: message,
+    html: `<p>${message}</p>`,
+  };
+
+  try {
+    await sgMail.send(content);
+    res.status(200).send('Message sent successfully.');
+  } catch (error) {
+    console.log('ERROR', error);
+    res.status(400).send('Message not sent.');
+  }
+}
+*/
+// SendGrid
--- pages/contact/form.js
+++ pages/contact/form.js
@@ -2,13 +2,28 @@ import Head from '../../components/head';
 import Header from '../../components/molecules/header';
 import Footer from '../../components/molecules/footer';
 
+// SendGrid
+/*
+fetch('/api/sendmail', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    email: 'test@exsample.com',
+    message: 'Test mail.',
+  }),
+});
+*/
+// SendGrid
+
 const Form = () => (
   <div>
     <Head title={'お問い合わせ（入力）'} url={'https://kagepedia.com/contact/form'} />
     <Header />
     <div className="p-4 bg-white rounded shadow">
-      <h1 className="text-2xl font-bold">Form</h1>
-      <p className="text-gray-500">with Tailwind CSS & postcss-preset-env. Now uses tailwind purge method for production builds.</p>
+      <h1 className="text-2xl font-bold">SendGrid Contact Form</h1>
+      <p className="text-gray-500">SendGrid</p>
     </div>
     <Footer />
   </div>
