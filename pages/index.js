a1e80b3bff471decc051c761865265db2eff12f3
Kage Soichiro <k4gepedia@gmail.com>
2020-09-26 14:38:50 +0900

    ローディングアニメーション追加

--- pages/index.js
+++ pages/index.js
@@ -9,13 +9,22 @@ import Profile from '../components/molecules/profile';
 import SearchForm from '../components/atom/SeachForm';
 import Footer from '../components/molecules/footer';
 
+// Loading
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleNotch } from '@fortawesome/free-solid-svg-icons';
+
 const Top = () => {
   const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     async function getPosts() {
+      setIsLoading(true);
       const allPosts = await fetchEntriesTop();
       setPosts([...allPosts]);
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 400);
     }
     getPosts();
   }, []);
@@ -26,19 +35,25 @@ const Top = () => {
       <Header />
       <div className="container mx-auto flex flex-wrap py-6">
         <section className="w-full md:w-2/3 flex flex-col items-center px-3">
-          {posts.length > 0
-            ? posts.map((p) => (
-                <PostList
-                  img_url={noImage(p).url}
-                  img_alt={noImage(p).title}
-                  title={p.fields.title}
-                  publishDate={formatDate(p.fields.publishDate)}
-                  discription={p.fields.discription}
-                  slug={p.fields.slug}
-                  key={p.fields.slug}
-                />
-              ))
-            : null}
+          {isLoading ? (
+            <div className="animate-spin text-6xl text-blue-800">
+              <FontAwesomeIcon icon={faCircleNotch} />
+            </div>
+          ) : posts.length > 0 ? (
+            posts.map((p) => (
+              <PostList
+                img_url={noImage(p).url}
+                img_alt={noImage(p).title}
+                title={p.fields.title}
+                publishDate={formatDate(p.fields.publishDate)}
+                discription={p.fields.discription}
+                slug={p.fields.slug}
+                key={p.fields.slug}
+              />
+            ))
+          ) : (
+            <p>現在表示できる記事はありません</p>
+          )}
         </section>
         <aside className="w-full md:w-1/3 flex flex-col items-center px-3">
           <Profile />
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -10,6 +10,10 @@ import Profile from '../../components/molecules/profile';
 import SearchForm from '../../components/atom/SeachForm';
 import Footer from '../../components/molecules/footer';
 
+// Loading
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleNotch } from '@fortawesome/free-solid-svg-icons';
+
 // _Pager
 import _Pager from '../../components/atom/_Pager';
 
@@ -23,6 +27,7 @@ const Post = () => {
   const [posts, setPosts] = useState([]);
   const [postsCount, setPostsCount] = useState(0);
   const [query, setQuesy] = useState();
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     // queryが利用可能になったら処理される
@@ -34,10 +39,14 @@ const Post = () => {
   useEffect(() => {
     // 関数の実行
     async function getPosts(query) {
+      setIsLoading(true);
       const allPosts = await fetchEntriesPost(limit, skip, query);
       const allPostsCounter = await fetchEntriesAllPostCount(query);
       setPosts([...allPosts]);
       setPostsCount(allPostsCounter);
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 400);
     }
     getPosts(query);
   }, [query]);
@@ -51,7 +60,11 @@ const Post = () => {
       <Header />
       <div className="container mx-auto flex flex-wrap py-6">
         <section className="w-full md:w-2/3 flex flex-col items-center px-3">
-          {posts.length > 0 ? (
+          {isLoading ? (
+            <div className="animate-spin text-6xl text-blue-800">
+              <FontAwesomeIcon icon={faCircleNotch} />
+            </div>
+          ) : posts.length > 0 ? (
             posts.map((p) => (
               <PostList
                 img_url={noImage(p).url}
@@ -66,7 +79,7 @@ const Post = () => {
           ) : (
             <p>表示する記事がありません</p>
           )}
-          {postsCount && lastPage !== 1 ? <_Pager page={page} total={postsCount} perPage={limit} href={`/posts/page/[page]`} asCallback={(page) => `/posts/page/${page}`} /> : ``}
+          {postsCount && lastPage !== 1 && !isLoading ? <_Pager page={page} total={postsCount} perPage={limit} href={`/posts/page/[page]`} asCallback={(page) => `/posts/page/${page}`} /> : ``}
         </section>
         <aside className="w-full md:w-1/3 flex flex-col items-center px-3">
           <Profile />
