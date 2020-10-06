9f753c916be91578edd3e76ca47816472eb71cc1
Kage Soichiro <k4gepedia@gmail.com>
2020-10-06 23:59:55 +0900

    abs path

--- components/molecules/profile.js
+++ components/molecules/profile.js
@@ -5,7 +5,7 @@ const Profile = () => (
     </div>
     <p className="text-xl text-center mt-3 font-semibold pb-5">Kage Soichiro</p>
     <p className="pb-2">WEB制作会社でWEBエンジニアとして働いている24歳です。技術ネタを中心に発信していきます。</p>
-    <a href="about" className="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-4">
+    <a href="/about" className="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-4">
       詳しいプロフィールはこちら
     </a>
   </div>
