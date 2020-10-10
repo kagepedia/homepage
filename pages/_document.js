e2b7d050dff9b688d91bfa00ab63aabef9a0cd73
Kage Soichiro <k4gepedia@gmail.com>
2020-10-11 00:31:05 +0900

    gadファイル修正

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { existsGaId, GTM_TRACKING_ID } from '../utils/gtm';
-import { existsAdClientId, AD_CLIENT_ID } from '../utils/gad';
+import { existsAdClientId, GAD_CLIENT_ID } from '../utils/gad';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -10,7 +10,6 @@ class MyDocument extends Document {
 
   render() {
     const GA_TRACKING_ID = GTM_TRACKING_ID;
-    console.log(GA_TRACKING_ID);
     const gtmScript = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GA_TRACKING_ID}');`
     const gtmFrame = `<iframe src="https://www.googletagmanager.com/ns.html?id=${GA_TRACKING_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
     
@@ -21,7 +20,7 @@ class MyDocument extends Document {
             <script dangerouslySetInnerHTML={{ __html: gtmScript }} />
           ) : null}
           {existsAdClientId ? (
-            <script data-ad-client={`${AD_CLIENT_ID}`} async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+            <script data-ad-client={`${GAD_CLIENT_ID}`} async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
           ) : null}
         </Head>
         <body>
--- utils/gad.js
+++ utils/gad.js
@@ -3,4 +3,4 @@ const AD_CLIENT_ID = process.env.AD_CLIENT_ID;
 // IDが取得できない場合を想定する
 export const existsAdClientId = AD_CLIENT_ID !== '';
 
-export const AD_CLIENT_ID = AD_CLIENT_ID ? AD_CLIENT_ID : '';
+export const GAD_CLIENT_ID = AD_CLIENT_ID ? AD_CLIENT_ID : '';
