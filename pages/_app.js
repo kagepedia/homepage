3ec9ec37643fb6d0d5e88e46efec6f7371934ff8
Kage Soichiro <k4gepedia@gmail.com>
2020-10-10 23:53:15 +0900

    gtm使用によりhooksによる収集を削除

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,5 @@
-import Router from 'next/router';
-import * as gtag from '../utils/gtag';
 import '../public/css/main.css';
 import 'highlight.js/styles/night-owl.css';
-import { useEffect } from 'react';
 
 // fontawesome
 import '@fortawesome/fontawesome-svg-core/styles.css';
@@ -10,21 +7,6 @@ import { config } from '@fortawesome/fontawesome-svg-core';
 config.autoAddCss = false;
 
 function App({ Component, pageProps }) {
-  useEffect(() => {
-    if (!gtag.existsGaId) {
-      return;
-    }
-
-    const handleRouteChange = (path) => {
-      gtag.pageview(path);
-    };
-
-    Router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      Router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, []);
-
   return <Component {...pageProps} />;
 }
 
